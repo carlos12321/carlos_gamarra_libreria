ba295185ee9c566fa4e509d536fa7e3d57bd2d4c
Carlos Gamarra <carlos.gamarra@goldenhippo.com>
2023-11-05 13:01:52 -0500

    update

---
+++ vars/sonarAnalysis.groovy
@@ -0,0 +1,22 @@
+def call(boolean abortPipeline = false, boolean waitForSonarResults = true) {
+    stage('SonarQube Analysis') {
+        steps {
+            script {
+                // Ejecuta el análisis de SonarQube
+                sh 'SonarScanner'
+                
+                if (waitForSonarResults) {
+                    // Espera durante 5 minutos por los resultados del análisis
+                    timeout(time: 5, unit: 'MINUTES') {
+                        def qualityGate = waitForQualityGate() // Función que verifica el QualityGate de SonarQube
+                        if (abortPipeline && qualityGate.status != 'OK') {
+                            error("QualityGate failed, aborting the pipeline.")
+                        }
+                    }
+                } else {
+                    echo 'SonarQube analysis completed.'
+                }
+            }
+        }
+    }
+}
